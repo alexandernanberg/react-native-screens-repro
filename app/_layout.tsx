d6415fc2fd661d5fe07b92bd28bb9e34fcbb5337
Alexander Nanberg <alex.nanberg@hotmail.com>
2023-12-02 08:13:54 +0100

    Repro

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import { Alert, Pressable, Text, useColorScheme } from 'react-native';
+import { View } from '../components/Themed';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -48,7 +53,35 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerLeft: () => (
+              <Pressable
+                style={{ backgroundColor: 'blue' }}
+                hitSlop={12}
+                onPress={() => {
+                  Alert.alert('Test');
+                }}
+              >
+                <Text style={{ color: 'white' }}>Press me</Text>
+              </Pressable>
+            ),
+            headerRight: () => (
+              <View style={{ paddingHorizontal: 12, marginHorizontal: -12 }}>
+                <Pressable
+                  style={{ backgroundColor: 'green' }}
+                  hitSlop={12}
+                  onPress={() => {
+                    Alert.alert('Test');
+                  }}
+                >
+                  <Text style={{ color: 'white' }}>Press me</Text>
+                </Pressable>
+              </View>
+            ),
+          }}
+        />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
     </ThemeProvider>
